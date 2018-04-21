af61261a5b98b2ceabdbcf5becc4d4137c0acc69
Guilherme Souza <guilherme.souza.silva95@gmail.com>
2018-04-21 11:16:23 -0300

    [ADD] Player Motor is 70% now

--- Assets/Scripts/InGame/PlayerMotor.cs
+++ Assets/Scripts/InGame/PlayerMotor.cs
@@ -1,7 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using CerfGames.Utils;
-public class PlayerMotor: MonoBehaviour {
+using UnityEngine;
+public class PlayerMotor : MonoBehaviour {
+    public float velocity = 2f;
+    private float oldVelocity;
+    private float movX, movY;
 
+    private Rigidbody2D playerBody;
+    private void Awake () {
+        oldVelocity = velocity;
+        playerBody = GetComponent<Rigidbody2D> ();
+    }
+    private void Update () {
+        movX = Input.GetAxis ("Horizontal");
+        movY = Input.GetAxis ("Vertical");
+        transform.Translate (movX * velocity * Time.deltaTime, movY * velocity * Time.deltaTime, 0);
+        if (Input.GetKeyDown (KeyCode.LeftShift) || Input.GetKeyDown (KeyCode.Joystick1Button1)) {
+            velocity *= 2f;
+        }
+        if (Input.GetKeyUp (KeyCode.LeftShift) || Input.GetKeyUp (KeyCode.Joystick1Button1)) {
+            velocity = oldVelocity;
+        }
+    }
 }
