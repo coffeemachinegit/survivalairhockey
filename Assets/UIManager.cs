a0f47a000cc29326a5d7219b5abcdeda73a84422
Gustavo de Souza Silva <xgustavossilva@gmail.com>
2018-04-21 16:02:31 -0300

    [Add] bracos dos personagens

---
+++ Assets/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using CerfGames.Utils;
+
+public class UIManager : Singleton<UIManager> {
+
+	//The canvas group from each UI Screen
+	public CanvasGroup startGameGroup;
+	public CanvasGroup gameGroup;
+	public CanvasGroup creditsGroup;
+	//--------------------------------------------
+	//Variables from UI Elements
+	public Slider hungrySlider,thristSlider,hpSlider; //All the on screen slider
+	public TextMeshProUGUI scoreBoardText; //The game Score
+	//--------------------------------------------
+	float time;
+
+	protected override void Awake() {
+		IsPersistentBetweenScenes = false;
+		base.Awake();
+	}
+
+	private void Update() {
+		
+	}
+
+	public void UpdateSliderValue(string flag,float value){
+		if(flag == "hungry"){
+			hungrySlider.value = value;
+		}else if(flag == "thirst"){
+			thristSlider.value = value;
+		}else{
+			hpSlider.value -= (int)value;
+		}
+	}
+
+	public void ChangeScore(int home, int visitor){
+		scoreBoardText.text = home+" x "+visitor;
+	}
+}
