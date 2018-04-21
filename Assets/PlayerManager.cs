e98babeb930511f0133327afdc1351dc0b41de4e
Gustavo de Souza Silva <xgustavossilva@gmail.com>
2018-04-21 11:13:04 -0300

    [Add] stats references

---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+	GameObject player;
+	PlayerStats playerStats;
+
+	private void Awake() {
+		playerStats = player.getComponenet<PlayerStats>();
+	}
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
