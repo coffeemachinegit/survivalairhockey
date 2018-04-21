a186fa8944d792fcab8d01f0d614d48a33e57c7d
Gustavo de Souza Silva <xgustavossilva@gmail.com>
2018-04-21 20:03:11 -0300

    [Add] add script to make a item collectable

--- Assets/SurvivalItem.cs
+++ Assets/SurvivalItem.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ItemType{Food,Liquid}
+
 public class SurvivalItem : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public int value;
+	public ItemType type;
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		if(other.tag == "Player"){
+			if(type == ItemType.Food){
+				PlayerManager.Instance.playerStats.Hungry += value;
+			}else{
+				PlayerManager.Instance.playerStats.Thirst += value;
+			}
+		}
+		gameObject.SetActive(false);
 	}
 }
