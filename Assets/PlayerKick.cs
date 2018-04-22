3db352c54691604afa058f999be78f4bbe618aab
Guilherme Souza <guilherme.souza.silva95@gmail.com>
2018-04-22 00:17:50 -0300

    [Add] New Kick Animation

--- Assets/PlayerKick.cs
+++ Assets/PlayerKick.cs
@@ -7,16 +7,20 @@ public class PlayerKick : MonoBehaviour {
 	Vector3 direction;
 	float distance;
 	private Rigidbody2D ball;
+
+	private PlayerAnimation animation;
 	[SerializeField]
 	private float _force,maxDistance=1f;
 
 	PlayerRotation refer;
 	private void Start () {
+		animation = GetComponentInChildren<PlayerAnimation>();
 		ball = GameManager.Instance.ball.GetComponent<Rigidbody2D>();
 		refer = GetComponentInChildren<PlayerRotation> ();
 
 	}
 	private void Update () {
+		animation.KickAnimation(false);
 		direction = refer.direction;
 		distance = refer.distance;
 		if (distance <= maxDistance) {
@@ -26,6 +30,7 @@ public class PlayerKick : MonoBehaviour {
 		}
 	}
 	void Shoot (Rigidbody2D ball, Vector2 dirToShoot) {
+		animation.KickAnimation(true);
 		ball.AddForce (dirToShoot * _force, ForceMode2D.Impulse);
 	}
 }
