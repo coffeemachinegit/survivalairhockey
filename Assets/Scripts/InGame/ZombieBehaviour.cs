d2d8e70402aaff9b391aae56e6a7f527f24a6667
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2018-04-21 23:08:30 -0300

    [UPDATE] Zombie com som

---
+++ Assets/Scripts/InGame/ZombieBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieBehaviour : MonoBehaviour {
+
+
+	[SerializeField] private float _damage;
+	[SerializeField] private AudioSource _audioSource;
+
+	void Awake()
+	{
+		_audioSource = GetComponentInChildren<AudioSource>();
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			_audioSource.Play();
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			_audioSource.Stop();
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			PlayerManager.Instance.TakeDamage(_damage * Time.deltaTime);
+		}
+	}
+}
