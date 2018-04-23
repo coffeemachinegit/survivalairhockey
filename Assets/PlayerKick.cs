d9997a57941018e8ebf6196ce8c40260eabcc505
Guilherme Souza <guilherme.souza.silva95@gmail.com>
2018-04-22 21:43:57 -0300

    [Update] Change Player kick behaviour

--- Assets/PlayerKick.cs
+++ Assets/PlayerKick.cs
@@ -10,31 +10,44 @@ public class PlayerKick : MonoBehaviour {
 
 	private PlayerAnimation animation;
 	[SerializeField]
-	private float _force,maxDistance=1f;
+	private float _force, maxDistance = 1f;
 
 	public AudioSource source;
 	public AudioClip kick;
 
 	PlayerRotation refer;
+	bool canKick = false;
 	private void Start () {
-		animation = GetComponentInChildren<PlayerAnimation>();
-		ball = GameManager.Instance.ball.GetComponent<Rigidbody2D>();
-		refer = GetComponentInChildren<PlayerRotation> ();
+		animation = GetComponent<PlayerAnimation> ();
+		ball = GameManager.Instance.ball.GetComponent<Rigidbody2D> ();
+		refer = GetComponent<PlayerRotation> ();
 
 	}
 	private void Update () {
-		animation.KickAnimation(false);
+		animation.KickAnimation (false);
 		direction = refer.direction;
 		distance = refer.distance;
-		if (distance <= maxDistance) {
-			if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.Joystick1Button0)) {
-				source.PlayOneShot(kick);
+		if (canKick) {
+			if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.Joystick1Button0) || Input.GetMouseButtonDown (0)) {
+				source.PlayOneShot (kick);
 				Shoot (ball, direction);
 			}
 		}
 	}
+
+	private void OnTriggerEnter2D (Collider2D other) {
+
+		if (other.gameObject.transform.name == "Ball") {
+			canKick = true;
+		}
+	}
+	private void OnTriggerExit2D (Collider2D other) {
+		if (other.gameObject.transform.name == "Ball") {
+			canKick = false;
+		}
+	}
 	void Shoot (Rigidbody2D ball, Vector2 dirToShoot) {
-		animation.KickAnimation(true);
+		animation.KickAnimation (true);
 		ball.AddForce (dirToShoot * _force, ForceMode2D.Impulse);
 	}
 }
